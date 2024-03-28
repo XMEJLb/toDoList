f3ea94bb9611b1160de073f59f6e9f0353cdbfe5
Den <khmell@ya.com>
2024-03-28 14:13:21 +0300

    40

---
+++ App.style.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+export const s = StyleSheet.create({
+  app: {
+    flex: 1,
+  },
+  header: {
+    flex: 1,
+  },
+  body: {
+    flex: 5,
+  },
+  footer: {
+    height: 70,
+  },
+});
